e552ea7ec1792198930cea97c8e60a1685afd380
Sayan Khutia <sayank.khutia123@gmail.com>
2026-06-12 19:34:23 +0530

    Meeting Page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -20,8 +20,14 @@ export const meetingsRouter = createTRPCRouter({
       const [existingMeeting] = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration:
+            sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))::integer`.as(
+              "duration",
+            ),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)),
         );
@@ -146,17 +152,17 @@ export const meetingsRouter = createTRPCRouter({
   remove: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      const [removedAgent] = await db
+      const [removedMeeting] = await db
         .delete(meetings)
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id)),
         )
         .returning();
 
-      if (!removedAgent) {
+      if (!removedMeeting) {
         throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
       }
 
-      return removedAgent;
+      return removedMeeting;
     }),
 });
